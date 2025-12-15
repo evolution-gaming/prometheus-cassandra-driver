8365796312bfc7f8c11045c3581be62a855d67a3
Oleg Nizhnikov <odomontois@gmail.com>
2025-12-15 12:56:40 +0100

    clean imports

--- src/main/java/com/evolutiongaming/prometheus/cassandra/TimerMetricFamilyBuilder.java
+++ src/main/java/com/evolutiongaming/prometheus/cassandra/TimerMetricFamilyBuilder.java
@@ -7,7 +7,6 @@ import io.prometheus.client.Collector.MetricFamilySamples.Sample;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import static com.evolutiongaming.prometheus.cassandra.Conversions.nsToSec;
 
 /* package */class TimerMetricFamilyBuilder {
